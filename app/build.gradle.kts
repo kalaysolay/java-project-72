c19b2e05c2218816c80b825d5070e89e338e1277
kalaysolay <ashat_boztaev@mail.ru>
2025-06-07 01:49:15 +0500

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("java")
+    id("checkstyle")
+    id("jacoco")
+    id("org.sonarqube") version "6.2.0.5505"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)    // ☑️ ОБЯЗАТЕЛЬНО для SonarQube
+        html.required.set(true)   // по желанию
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "kalaysolay_java-project-72")
+        property("sonar.organization", "kalaysolay")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+    }
+}
